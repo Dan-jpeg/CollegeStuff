6338b595047de19a258194446e10c7d1f0b7d0cf
DanGribanovsky <dangrib99@gmail.com>
2020-11-29 16:57:49 +0300

    Create ItemScale.cs

---
+++ Inventory2/ItemScale.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(CanvasGroup))]
+
+public class ItemScale : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    public AudioSource click;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        click.Play();
+        Debug.Log("Click");
+    }
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        rectTransform.sizeDelta *= 1.5f;
+        Debug.Log("Cursor on our Image!");
+    }
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        rectTransform.sizeDelta /= 1.5f;
+        Debug.Log("We lost the Cursor!");
+    }
+
+}
